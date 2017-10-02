269cd2e8d30751b398a54dc90535ea000932deeb
Brendan White <bmwt@es.net>
2017-10-01 20:07:16 -0700

    clean up

--- Arduino/weightduino/weightduino.ino
+++ Arduino/weightduino/weightduino.ino
@@ -1,87 +1,63 @@
 #include "HX711.h"
 #include "SSD1306.h"
-
-
 HX711 scale;
 SSD1306  display(0x3c, 5, 4);
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Weightduino");
   display.init();
   display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_16);
+  
   Serial.println("Initializing the scale");
+  display.setFont(ArialMT_Plain_16);
   display.clear();
   display.drawString(0, 26, "Initializing Scale");
   display.setTextAlignment(TEXT_ALIGN_CENTER);
   display.drawString(10, 128, String(millis()));
   display.display();
 
-  
-  // parameter "gain" is ommited; the default value 128 is used by the library
-  // HX711.DOUT  - pin #A1
-  // HX711.PD_SCK - pin #A0
+  // data pins from HX711 connected to 25 and 26 on esp.  default gain (128)
   scale.begin(25, 26);
-
-  Serial.println("Before setting up the scale:");
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());     // print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scale.read_average(20));   // print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight (not set yet)
-
-  Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);  // print the average of 5 readings from the ADC minus tare weight (not set) divided
-            // by the SCALE parameter (not set yet)
-
-  scale.set_scale(11366.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
- // scale.set_scale(1.f);  //  use to calibrate.  RAW_WEIGHT % KNOWN_WEIGHT = SCALE_FACTOR
-  scale.tare();               // reset the scale to 0
-
-  Serial.println("After setting up the scale:");
-
-  Serial.print("read: \t\t");
-  Serial.println(scale.read());                 // print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight, set with tare()
-
-  Serial.print("get units: \t\t");
-  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
-            // by the SCALE parameter set with set_scale
-
-  Serial.println("Readings:");
+  Serial.println("Pre-Callibration:");
+  Serial.print("raw ADC reading (1): ");
+  Serial.println(scale.read());
+  Serial.print("raw ADC reading (average of 20)");
+  Serial.println(scale.read_average(20));
+  Serial.print("raw ADC minus tare (average of 5, tare not yet set): ");
+  Serial.println(scale.get_value(5));
+  Serial.print("raw ADC minus tare, divided by scale (average of 5, not yet set): ");
+  Serial.println(scale.get_units(5), 1);
+  //set scaling factor
+  scale.set_scale(11366.f);
+  // use the following to re-calibrate.  RAW_WEIGHT % KNOWN_WEIGHT = SCALE_FACTOR
+  // scale.set_scale(1.f); 
+  scale.tare();
+  Serial.print("raw ADC minus tare (average of 5): ");
+  Serial.println(scale.get_value(5));
+  Serial.print("raw ADC minus tare, divided by scale (average of 5): ");
+  Serial.println(scale.get_units(5), 1);
 }
 
-
 void displayWeight(float weight) {
-   display.setFont(ArialMT_Plain_24);
-   display.setTextAlignment(TEXT_ALIGN_LEFT);
    String stringSuffix = " lbs";
    String displayString = weight + stringSuffix;
+   display.clear();
+   display.setTextAlignment(TEXT_ALIGN_LEFT);
    display.setFont(ArialMT_Plain_16);
    display.drawString(0, 0, "Weightduino 0.1");
    display.setFont(ArialMT_Plain_24);
    display.drawString(0, 18, displayString);
+   display.display();
    Serial.println(displayString);
+   
 }
+
 void loop() {
-  float weight = scale.get_units(10);
-  display.clear();
-  displayWeight(abs(weight));
-  display.setTextAlignment(TEXT_ALIGN_RIGHT);
-  display.drawString(10, 128, String(millis()));
-  display.display();
-  Serial.print("Weight: ");
-  Serial.print(weight);
-  Serial.print(" lbs");
-  scale.power_down();             // put the ADC in sleep mode
-  delay(5000);
   scale.power_up();
+  float weight = scale.get_units(10);
+  displayWeight(abs(weight));
+  scale.power_down();             // put the ADC in sleep mode
+  delay(1000);
 }
+
